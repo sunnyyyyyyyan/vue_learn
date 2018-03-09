d859d40ffc2de3e3c2e08b11d1c0923bff49fb1e
sunny <993303027@qq.com>
2018-03-09 22:23:02 +0800

    完成常用指令学习

--- js/testPro.js
+++ js/testPro.js
@@ -10,8 +10,29 @@ let vm = new Vue({
         height1:1.50,
         score:90,   //优秀，良好，及格，
         scores:[100,88,87,80],
-        dog:{name:'sunny',age:7,height:3.2},
+        dog:{ name:'sunny',age:7,height:3.2 },
         name:'chen',
-        dig:'1293'
+        dig:'1293',
+        persons:[
+            { name:'a',age:7,sex:'man' },
+            { name:'b',age:8,sex:'woman' },
+            { name:'c',age:9,sex:'man' },
+            { name:'d',age:10,sex:'woman' }
+        ],
+        html:'<input type="date"><input type="color">',
+        imgSrc:'img/a.jpg',
+        imgSrc1:'img/b.jpg',
+        alt:'sunny',
+        lips:[ 'ios学院','ui学院','html学院','java学院','c++学院','android学院'],
+        fontColor:'red'
+    },
+    methods:{   //实例所有的函数实现
+        changeContent(){
+            //alert(0);
+            this.msg='改掉了';
+        },
+        changeContentColor(){
+            this.fontColor='green';
+        }
     }
 });
